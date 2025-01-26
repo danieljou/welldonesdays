8133fe043aa87200830a0c8743a3ac6a6c56e53c
danieljou <danielsitepro@gmail.com>
2025-01-27 00:15:34 +0100

    react/no-unescaped-entities

--- src/app/(site)/inscription/page.tsx
+++ src/app/(site)/inscription/page.tsx
@@ -22,17 +22,17 @@ const ProgramPage = () => {
         <div className="flex flex-col md:flex-row justify-center gap-4">
           <Link href="https://docs.google.com/forms/d/e/1FAIpQLSfL7kQ-7KpAARoqoSk1bclrFUG0zXQr2dnSp8kY3yuaxcRmDA/viewform?usp=header" target="_blank" passHref>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full md:w-auto">
-              S'inscrire en tant que Participant
+              {"S'inscrire en tant que Participant"}
             </button>
           </Link>
           <Link href="https://docs.google.com/forms/d/e/1FAIpQLSfJ3QolD0KyuK0yViNe36o4M5bbWnhd8JfrySafKv11MGgeaQ/viewform?usp=header" target="_blank" passHref>
             <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full md:w-auto">
-              Devenir Sponsor
+              {"Devenir Sponsor"}
             </button>
           </Link>
           <Link href="https://docs.google.com/forms/d/e/1FAIpQLScIbYvYSyqEHZJFiQ26viDZGIXrxOxL-F6yuDNsrW2Wi3BpEw/viewform?usp=header" target="_blank" passHref>
             <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded w-full md:w-auto">
-              Devenir Partenaire
+             {" Devenir Partenaire"}
             </button>
           </Link>
         </div>
